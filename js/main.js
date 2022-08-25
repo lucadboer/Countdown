0a57e352f4f32f4db7d02875932090f64f68ea37
Luca Destefano Boer <lucahesper@hotmail.com>
2022-08-25 15:07:31 -0300

    up files

--- js/main.js
+++ js/main.js
@@ -1,9 +1,39 @@
 const time = document.getElementById('time')
+const buttonOpen = document.querySelector('.subscribe')
+const modal = document.querySelector('.modal-subscribe')
+const buttonClose = document.querySelector('#btnClose')
+const form = document.querySelector('#form-subscribe')
 
 let days = 10;
 let hours = 4;
 let minutes = 1;
 let seconds = 5;
+
+buttonOpen.addEventListener('click', () => {
+    modal.classList.remove('modal-disable')
+    modal.classList.add('modal-active')
+})
+
+buttonClose.addEventListener('click', () => {
+    modal.classList.add('modal-disable')
+})
+
+modal.addEventListener('click', (event) => {
+    console.log(event.target.id);
+    if (event.target.id == 'form-subscribe') {
+        modal.classList.add('modal-disable')
+    }
+})
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    swal("Cadastro realizado!", "", "success")
+    .then(() => {
+        modal.classList.add('modal-disable')
+    })
+
+})
+
 
 function add0(num) {
     if (num < 10) {
@@ -44,5 +74,7 @@ function timer() {
 }
 
 timer()
+
+
 
 
